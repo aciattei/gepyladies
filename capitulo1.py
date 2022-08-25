7c7c48fa5fd9e1d08ec3d8e96dc41abd6f2cd1c5
Ariane <aciattei@gmail.com>
2022-08-25 14:39:30 -0300

    Inclusão de exercícios do 1º capítulo

---
+++ capitulo1.py
@@ -0,0 +1,36 @@
+#Exercício 1.1
+
+print('Hello, World!')
+#1.1.1
+print('Hello, World!'
+print'Hello, World!'
+#1.1.2
+print(Hello, World!)
+print('Hello, World!)
+#1.1.3
+print(2+2)
+print(-2+2)
+print(2++2)
+#1.1.4
+print(2+02)
+#1.1.5
+print(2 2)
+
+#1.2
+#1.2.1
+minutos = 42
+segundos = 42
+tempo = (minutos*60)+42
+print('Tempo = ' + str(tempo))
+
+#1.2.2
+km = 10
+milhas = km*1.61
+print('Distancia em milhas = ' + str(milhas))
+
+
+#1.2.3
+segundosTotais = minutos * 60 + segundos
+horas = segundosTotais/360
+VelocidadeMedia = milhas/horas
+print('A velocidade média é de {:.2f}'.format(VelocidadeMedia), 'milhas/h')
